db052a7b6a19b2fe602ca61d4fe0b454a6374338
kracas <karolis@racevicius.lt>
2024-04-20 17:26:00 +0200

    cuda 3

--- types/backends/onnx.d.ts
+++ types/backends/onnx.d.ts
@@ -1,5 +1,5 @@
-/** @type {import('onnxruntime-web')} The ONNX runtime module. */
-export let ONNX: typeof ONNX_WEB;
+/** @type {import('onnxruntime-node')} The ONNX runtime module. */
+export let ONNX: typeof ONNX_NODE;
 export const executionProviders: string[];
-import * as ONNX_WEB from 'onnxruntime-web';
+import * as ONNX_NODE from 'onnxruntime-node';
 //# sourceMappingURL=onnx.d.ts.map
